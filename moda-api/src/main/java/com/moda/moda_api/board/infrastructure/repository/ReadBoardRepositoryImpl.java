ede87b69361bb6af9896d5e9de04eb9422678f6b
김강토 <kangsansam@naver.com>
2025-02-03 21:02:21 +0900

    :sparkles: Feat. 보드 확인 상태 변환 구현

--- moda-api/src/main/java/com/moda/moda_api/board/infrastructure/repository/ReadBoardRepositoryImpl.java
+++ moda-api/src/main/java/com/moda/moda_api/board/infrastructure/repository/ReadBoardRepositoryImpl.java
@@ -5,11 +5,13 @@ import com.moda.moda_api.board.domain.ReadBoardRepository;
 import com.moda.moda_api.board.infrastructure.entity.ReadBoardEntity;
 import com.moda.moda_api.user.domain.UserId;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class ReadBoardRepositoryImpl implements ReadBoardRepository {
@@ -30,11 +32,13 @@ public class ReadBoardRepositoryImpl implements ReadBoardRepository {
 
     @Override
     public void save(UserId userId, BoardId boardId) {
-        ReadBoardEntity entity = ReadBoardEntity.builder()
+        log.info("ReadBoardEntity 저장 전 - userId: {}, boardId: {}",
+                userId.getValue(), boardId.getValue());
+        ReadBoardEntity savedEntity = ReadBoardEntity.builder()
                 .userId(userId.getValue())
                 .boardId(boardId.getValue())
                 .build();
-
-        readBoardJpaRepository.save(entity);
+        log.info("ReadBoardEntity 저장 후 - userId: {}, boardId: {}", savedEntity.getUserId(), savedEntity.getBoardId());
+        readBoardJpaRepository.save(savedEntity);
     }
 }
