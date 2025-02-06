13a797665caf67485f7a740e7bbdf743174f5eb3
김강토 <kangsansam@naver.com>
2025-02-06 23:47:35 +0900

    :sparkles: Feat. 매퍼 및 검색 메서드 구현

--- moda-api/src/main/java/com/moda/moda_api/card/infrastructure/entity/CardDocumentEntity.java
+++ moda-api/src/main/java/com/moda/moda_api/card/infrastructure/entity/CardDocumentEntity.java
@@ -1,18 +1,35 @@
 package com.moda.moda_api.card.infrastructure.entity;
 
 import jakarta.persistence.Id;
+import lombok.*;
 import org.springframework.data.elasticsearch.annotations.*;
 
 @Document(indexName = "card")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Setting(settingPath = "elasticsearch/settings.json")
 @Mapping(mappingPath = "elasticsearch/mappings.json")
 public class CardDocumentEntity {
     @Id
-    private String id;
+    private String id;  // cardId를 사용
+
+    @Field(type = FieldType.Text, analyzer = "korean")
+    private String title;
 
     @Field(type = FieldType.Text, analyzer = "korean")
     private String content;
 
     @Field(type = FieldType.Text, analyzer = "korean_completion")
-    private String contentCompletion;
+    private String titleCompletion;  // 제목 자동완성용
+
+    @Field(type = FieldType.Text, analyzer = "korean_completion")
+    private String contentCompletion;  // 내용 자동완성용
+
+    @Field(type = FieldType.Keyword)
+    private String[] keywords;
+
+    @Field(type = FieldType.Dense_Vector, dims = 768)
+    private float[] embedding;
 }
--- moda-api/src/main/java/com/moda/moda_api/card/infrastructure/repository/CardDocumentRepositoryImpl.java
+++ moda-api/src/main/java/com/moda/moda_api/card/infrastructure/repository/CardDocumentRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.moda.moda_api.card.infrastructure.repository;
 
 import com.moda.moda_api.card.domain.CardDocumentRepository;
+import com.moda.moda_api.card.infrastructure.entity.CardDocumentEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +12,12 @@ import java.util.List;
 public class CardDocumentRepositoryImpl implements CardDocumentRepository {
     private final CardDocumentJpaRepository cardDocumentJpaRepository;
 
-
     @Override
     public List<String> findAutoCompleteSuggestions(String prefix) {
-        return null;
+        return cardDocumentJpaRepository.findAutoCompleteSuggestions(prefix)
+                .stream()
+                .map(CardDocumentEntity::getContent)
+                .limit(5)  // 상위 5개 결과만
+                .toList();
     }
 }
