58e2678901de6d1dfe22e865f49971378bc48591
JongWon <jongwon990806@gmail.com>
2025-02-16 12:50:28 +0900

    AI연결

--- moda-api/src/main/java/com/moda/moda_api/crawling/infrastructure/config/crawlerConfig/PlatformExtractorFactory.java
+++ moda-api/src/main/java/com/moda/moda_api/crawling/infrastructure/config/crawlerConfig/PlatformExtractorFactory.java
@@ -9,96 +9,121 @@ import com.moda.moda_api.crawling.domain.model.UrlDomainType;
 
 @Component
 public class PlatformExtractorFactory {
-	private static final List<ExtractorConfig> CONFIGS = new ArrayList<>();
+	private static final List<ExtractorConfig> CONFIGS = initializeConfigs();
 
+	private static List<ExtractorConfig> initializeConfigs() {
+		List<ExtractorConfig> configs = new ArrayList<>();
+
+		// Mobile configurations
+		configs.add(createMobileNaverBlogConfig());
+		configs.add(createMobileTistoryConfig());
+
+		// Desktop configurations
+		configs.add(createDesktopNaverBlogConfig());
+		configs.add(createDesktopTistoryConfig());
+		// configs.add(createNaverNewsConfig());
+		configs.add(createVelogConfig());
+		configs.add(createDaumNewsConfig());
+		configs.add(createGoogleSearchConfig());
+
+		return configs;
+	}
 	//////////////////////////////////////////////////////////////////////////////
 	// 무조건 순서는 모바일 먼저다!!!!!!!!!!!!!///////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
 
-	static {
-
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createMobileNaverBlogConfig() {
+		return ExtractorConfig.builder()
 			.pattern("m.blog.naver.com")
 			.contentSelector(".se-main-container")
 			.imageSelector(".se_mediaImage")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.NAVER_BLOG)
-			.build());
+			.build();
+	}
 
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createMobileTistoryConfig() {
+		return ExtractorConfig.builder()
 			.pattern(".tistory.com/m")
 			.contentSelector(".blogview_content")
 			.imageSelector(".blogview_content img")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.TISTORY)
-			.build());
+			.build();
+	}
 
 		//////////////////////////////////////////////////////////////
 		////////////여기 위로는 모바일 밑으로는 pc////////////////////////
 		/////////////////////////////////////////////////////////////
+		private static ExtractorConfig createDesktopNaverBlogConfig() {
+			return ExtractorConfig.builder()
+				.pattern("blog.naver.com")
+				.contentSelector(".se-main-container")
+				.imageSelector(".se-image img, .se-module-image img")
+				.requiresFrame(true)
+				.frameId("mainFrame")
+				.urlDomainType(UrlDomainType.NAVER_BLOG)
+				.build();
+		}
 
-		CONFIGS.add(ExtractorConfig.builder()
-			.pattern("blog.naver.com")
-			.contentSelector(".se-main-container")
-			.imageSelector(".se-image img, .se-module-image img")
-			.requiresFrame(true)
-			.frameId("mainFrame")
-			.urlDomainType(UrlDomainType.NAVER_BLOG)
-			.build());
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createDesktopTistoryConfig() {
+		return ExtractorConfig.builder()
 			.pattern(".tistory.com")
 			.contentSelector(".contents_style")
 			.imageSelector(".contents_style img")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.TISTORY)
-			.build());
+			.build();
+	}
 
-		CONFIGS.add(ExtractorConfig.builder()
-			.pattern("n.news.naver.com")
-			.contentSelector("#dic_area, .go_trans._article_content")
-			.imageSelector("#dic_area img, .go_trans._article_content img")
-			.requiresFrame(false)
-			.urlDomainType(UrlDomainType.NAVER_NEWS)
-			.build()
-		);
+	// private static ExtractorConfig createNaverNewsConfig() {
+	// 	return ExtractorConfig.builder()
+	// 		.pattern("n.news.naver.com")
+	// 		.contentSelector("#dic_area, .go_trans._article_content")
+	// 		.imageSelector("#dic_area img, .go_trans._article_content img")
+	// 		.requiresFrame(false)
+	// 		.urlDomainType(UrlDomainType.NAVER_NEWS)
+	// 		.build();
+	// }
 
-		// Tistory 설정
-		CONFIGS.add(ExtractorConfig.builder()
-			.pattern(".tistory.com")
-			.contentSelector(".contents_style")
-			.imageSelector(".contents_style img")
-			.requiresFrame(false)
-			.urlDomainType(UrlDomainType.TISTORY)
-			.build());
-
-		// Velog 설정
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createVelogConfig() {
+		return ExtractorConfig.builder()
 			.pattern("velog.io")
 			.contentSelector(".sc-dFtzxp")
 			.imageSelector(".sc-dFtzxp img")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.VELOG)
-			.build());
+			.build();
+	}
 
-		// 다음 뉴스 설정
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createDaumNewsConfig() {
+		return ExtractorConfig.builder()
 			.pattern("v.daum.net")
 			.contentSelector(".article_view")
 			.imageSelector(".article_view img")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.DAUM_NEWS)
-			.build());
+			.build();
+	}
 
-		// 서치 설정.
-		CONFIGS.add(ExtractorConfig.builder()
+	private static ExtractorConfig createGoogleSearchConfig() {
+		return ExtractorConfig.builder()
 			.pattern("google.com")
-			// 더 구체적이고 정확한 셀렉터 사용
 			.contentSelector("div.MjjYud a[jsname='UWckNb']")
 			.urlSelector("div.MjjYud a[jsname='UWckNb'][href]")
 			.requiresFrame(false)
 			.urlDomainType(UrlDomainType.GOOGLE_SEARCH)
-			.build());
+			.build();
+	}
 
+	private static ExtractorConfig createDefaultConfig() {
+		return ExtractorConfig.builder()
+			.pattern("default")
+			.contentSelector("body")
+			.imageSelector("img")
+			.requiresFrame(false)
+			.urlDomainType(UrlDomainType.UNCLASSIFIED)
+			.build();
 	}
 
 	// 하나씩 꺼내보면서 맞는 사이트가 있는지 찾아보는 과정
@@ -108,17 +133,7 @@ public class PlatformExtractorFactory {
 		return CONFIGS.stream()
 			.filter(config -> lowerUrl.contains(config.getPattern()))
 			.findFirst()
-			.orElse(getDefaultConfig());
-	}
-
-	private ExtractorConfig getDefaultConfig() {
-		return ExtractorConfig.builder()
-			.pattern("default")
-			.contentSelector("body")
-			.imageSelector("img")
-			.requiresFrame(false)
-			.urlDomainType(UrlDomainType.UNCLASSIFIED)
-			.build();
+			.orElse(createDefaultConfig());
 	}
 
 }
