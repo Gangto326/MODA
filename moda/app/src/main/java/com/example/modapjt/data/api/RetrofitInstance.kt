70f16a363b1aeedcf2171f277bf13e0620fc785e
JongWon <jongwon990806@gmail.com>
2025-02-18 10:42:34 +0900

    merge conflict

--- moda/app/src/main/java/com/example/modapjt/data/api/RetrofitInstance.kt
+++ moda/app/src/main/java/com/example/modapjt/data/api/RetrofitInstance.kt
@@ -9,8 +9,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 //    private const val BASE_URL = "http://localhost:8080/" // API 서버 주소 입력
 
-    private const val BASE_URL = "http://10.0.2.2:8080" // localhost → 10.0.2.2 변경 (에뮬레이터 전용)
-//    private const val BASE_URL = "https://i12a805.p.ssafy.io" // 우리 서버!
+//    private const val BASE_URL = "http://10.0.2.2:8080" // localhost → 10.0.2.2 변경 (에뮬레이터 전용)
+    private const val BASE_URL = "https://i12a805.p.ssafy.io" // 우리 서버!
 //    private const val BASE_URL = "http://0.0.0.0:8080"  // 내 PC의 IP 주소로 변경
     private lateinit var tokenManager: TokenManager
 
@@ -67,6 +67,15 @@ object RetrofitInstance {
         createRetrofit().create(SignUpApiService::class.java)
     }
 
+    // 아이디 찾기 API
+    val findIdApi: FindIdApiService by lazy {
+        createRetrofit().create(FindIdApiService::class.java)
+    }
+
+    // 비밀번호 찾기 API
+    val findPasswordApi: FindPasswordApiService by lazy {
+        createRetrofit().create(FindPasswordApiService::class.java)
+    }
 
 //    val api: CategoryApiService by lazy {
 //        Retrofit.Builder()
