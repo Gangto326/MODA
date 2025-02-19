d16625193535a0671ada4d456b8d9e2ab9fc5db6
01HongNa <nayeon010508@naver.com>
2025-02-19 09:54:23 +0900

    💡 Update. 카드 업로드 화면 다크모드 적용

--- moda/app/src/main/java/com/example/modapjt/components/bar/LinkAddHeaderBar.kt
+++ moda/app/src/main/java/com/example/modapjt/components/bar/LinkAddHeaderBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.Text
@@ -28,7 +29,7 @@ fun LinkAddHeaderBar(
         modifier = modifier
             .fillMaxWidth() // 입력창이 화면 너비를 꽉 채우도록 설정
             .height(56.dp) // 높이를 56dp로 설정
-            .border(1.dp, Color.Gray, RoundedCornerShape(16.dp))  // 테두리 두께 1dp, 색상은 회색
+            .border(1.dp, MaterialTheme.colorScheme.onSecondary, RoundedCornerShape(16.dp))  // 테두리 두께 1dp, 색상은 회색
             .padding(horizontal = 16.dp) // 양옆 여백 16dp설정
 //            .padding(vertical = 8.dp) // 위,아래 여백 8dp설정
             .horizontalScroll(rememberScrollState()), // ✅ 긴 URL일 경우 가로 스크롤 가능하게 설정
@@ -41,8 +42,8 @@ fun LinkAddHeaderBar(
 
 
         colors = TextFieldDefaults.colors(
-            unfocusedContainerColor = Color.White, // 포커스가 없을 때 배경색
-            focusedContainerColor = Color.White, // 포커스가 있을 때 배경색
+            unfocusedContainerColor = MaterialTheme.colorScheme.tertiary, // 포커스가 없을 때 배경색
+            focusedContainerColor = MaterialTheme.colorScheme.tertiary, // 포커스가 있을 때 배경색
             unfocusedIndicatorColor = Color.Transparent, // 포커스가 없을 때 아래쪽 인디케이터 색상 (투명)
             focusedIndicatorColor = Color.Transparent // 포커스가 있을 때 아래쪽 인디케이터 색상 (투명)
         ),
