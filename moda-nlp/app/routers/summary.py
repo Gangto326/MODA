7649d9d0eb1a88f345d72ea5d9545008c24d1cc9
김종헌 <kimjjong321@naver.com>
2025-02-19 14:15:59 +0900

    :bug: Fix. 로그

--- moda-nlp/app/routers/summary.py
+++ moda-nlp/app/routers/summary.py
@@ -19,7 +19,7 @@ router = APIRouter(
 @router.post("/post")
 async def summary_document(post_request: PostRequest):
     try:
-        print("포스트 요약 시작 ")
+        print("포스트 요약 시작\n" + post_request.content)
         start_time = time.time()
 
         summarizer = PostSummary(post_request.content)
